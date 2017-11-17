5d8fedbaa5c07940c00d4be585ab1fe332e772fd
Jacquelyn Turcinovic <jacquelynturcinovic@wireless-10-146-188-95.public.utexas.edu>
2017-11-17 16:52:42 -0600

    updated JSON detection

--- d2s-varscript.py
+++ d2s-varscript.py
@@ -7,43 +7,64 @@ from os import listdir, getcwd
 from re import search
 from agavepy.actors import get_context, get_client
 
+def get_vars_env(agave_context):
+    '''Checks that MSG, system, and outdir are present as
+    context variables and returns their values'''
+    required_keyset = set(['message_dict', 'system', 'outdir'])
+    keyset = set(dict(agave_context).keys())
+    assert required_keyset.issubset(keyset), 'Context is missing required keys (MSG, system, outdir): {}'.format(dict(agave_context).keys())
+    return [ agave_context[x] for x in required_keyset ]
+
+def get_vars_json(agave_context):
+    '''Checks that container, system, and outdir are present
+    in message_dict and returns their values'''
+    mdict = context.message_dict
+    required_keyset = set(['container', 'system', 'outdir'])
+    keyset = set(mdict.keys())
+    assert required_keyset.issubset(keyset), 'Context is missing required keys (MSG, system, outdir): {}'.format(mdict.keys())
+    return [ mdict[x] for x in required_keyset ]
+
+def get_vars(agave_context):
+    '''Returns container, system, and outdir variables'''
+    # check if passed as JSON
+    if type(context.message_dict) is dict:
+        c, s, o = get_vars_json(agave_context)
+    else:
+        c, s, o = get_vars_env(agave_context)
+    return (c, s, o)
+
 if __name__ == '__main__':
 
     # get context and client
     context = get_context()
     ag = get_client()
 
-    # get container, agave system, and path to outdir
-    container = str(context['MSG']).replace("'", "")
-    assert ' ' not in container, 'Spaces in container name: '+container
-
-    system = str(context['system'])
-    assert ' ' not in system, 'Spaces in system name: '+system
-
-    outdir = str(context['outdir'])
-    assert ' ' not in outdir, 'Spaces in outdir path: '+outdir
-
-    print 'INITIAL FILES:\n  '+'\n  '.join(listdir('.'))
+    # container, agave system, and path to outdir 
+    container, system, outdir = get_vars(context)
 
     # execute d2s with bash
-    print '\nRUNNING CONTAINER'
-    bashcmd = 'bash /docker2singularity.sh '+container
+    print 'RUNNING CONTAINER', container
+    bashcmd = 'bash /docker2singularity.sh {}'.format(container)
     process = subprocess.Popen(bashcmd.split()).wait()
-    print 'Return code:', str(process)
     assert int(process) == 0, 'Command finished with non-zero status: '+str(process)
 
     # find image file produced
-    print '\nOUTPUT FILES:\n  '+'\n  '.join(listdir('/output/'))
+    print '\nFINDING IMG FILE'
     files = ' '.join(listdir('/output'))
     regex = container.replace('/', '_').replace(':', '_') + '-[0-9]{4}-[0-9]{2}-[0-9]{2}-\w{12}\.img'
-    print '\nREGEX:', regex
 
     img_search = search(regex, files)
     assert img_search is not None, 'Image for container '+container+' not found in files: '+files
+
     img = '/output/'+str(img_search.group(0))
-    print '\n\nIMG:', img
+    print img
 
     # upload img to desired system with agavepy
+    print '\nUPLOADING FILE'
+    print 'System ID: {}'.format(system)
+    print 'Path: {}'.format(outdir)
     file_upload = ag.files.importData(systemId = system,
                                       filePath = outdir,
                                       fileToUpload = open(img))
+
+    print '\nPROCESS COMPLETE'
