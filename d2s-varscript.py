e8d29a18654c03c6f069a5c344a23d41b34c6e05
Jacquelyn Turcinovic <jacquelynturcinovic@wireless-10-146-224-203.public.utexas.edu>
2017-12-12 08:54:27 -0600

    trying to find problem v3

--- d2s-varscript.py
+++ d2s-varscript.py
@@ -61,10 +61,19 @@ if __name__ == '__main__':
 
     # DEBUGGING
     print '\nDEBUGGING'
+    debugcmds = [ 'date', 'df -h', 'docker ps', 'docker images' ]
+    output = ''
+    for i in debugcmds:
+        p = subprocess.Popen(i.split(), stdout=subprocess.PIPE)
+        output = output+'\n'+p.stdout.read()
+    print output
+
     debugfile = str(container)+'_diagnostics.txt'
-    debugcmd = 'date > '+debugfile+'; df -h >> '+debugfile+'; docker ps >> '+debugfile+'; docker images >> '+debugfile+'; cat '+debugfile+'; mv '+debugfile+' /work/03761/jturcino/projects/docker/abaco-biocontainers/'
-    process = subprocess.Popen(debugcmd.split()).wait()
-    assert int(process) == 0, 'Debugging command finished with non-zero status: '+str(process)
+    with open(debugfile, 'w') as f:
+        f.write(output)
+    movecmd = 'mv '+debugfile+' /work/03761/jturcino/projects/docker/abaco-biocontainers/'
+    process = subprocess.Popen(movecmd.split()).wait()
+    assert int(process) == 0, 'Move command finished with non-zero status: '+str(process)
 
     # upload img to desired system with agavepy
     print '\nUPLOADING FILE'
