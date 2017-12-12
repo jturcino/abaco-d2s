b4c51826ea91a28b1fb233eca178dba26ba5ed2f
Jacquelyn Turcinovic <jacquelynturcinovic@wireless-10-146-224-203.public.utexas.edu>
2017-12-12 07:49:36 -0600

    trying to find problem

--- d2s-varscript.py
+++ d2s-varscript.py
@@ -59,6 +59,13 @@ if __name__ == '__main__':
     img = '/output/'+str(img_search.group(0))
     print img
 
+    # DEBUGGING
+    print '\nDEBUGGING'
+    debugfile = str(container)+'_diagnostics.txt'
+    debugcmd = 'date > '+debugfile'; df -h >> '+debugfile+'; docker ps >> '+debugfile+'; docker images >> '+debugfile+'; cat '+debugfile+'; mv '+debugfile+' /work/03761/jturcino/projects/docker/abaco-biocontainers/'
+    process = subprocess.Popen(debugcmd.split()).wait()
+    assert int(process) == 0, 'Debugging command finished with non-zero status: '+str(process)
+
     # upload img to desired system with agavepy
     print '\nUPLOADING FILE'
     print 'System ID: {}'.format(system)
